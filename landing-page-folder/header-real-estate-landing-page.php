eacf65eb2d304fb2350f75453cc954f5892af6d8
Don <donventura29@gmail.com>
2019-04-09 18:23:29 -0700

    Updating Landing Page for more modular setup

---
+++ landing-page-folder/header-real-estate-landing-page.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The header for Real Estate Landing Page
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <title>Sell Your Home Now with Signa Marketing</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+  
+  <link href="https://fonts.googleapis.com/css?family=Heebo:300,400,700,800,900" rel="stylesheet">
+  <script src="https://use.fontawesome.com/52dda3483b.js"></script>
+  <script defer src="/wp-content/themes/twentynineteen/landing-page-folder/js/vimeo.ga.min.js"></script>
+  <?php wp_head(); ?>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap-grid.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="/wp-content/themes/twentynineteen/landing-page-folder/landing.css" crossorigin="anonymous"><!-- Make sure this path is correct IT WILL CHANGE -->
+  
+</head>
+
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+<header id="plain-header" class="header-background container-fluid">
+            <div class="row" style="    max-width: 1140px; width: 100%; float: none; margin: auto;">
+    
+                <div class="col-12 col-sm-6">
+                    <a class="logo-link" href="https://flatfeeteamaz.com/" alt="Flat Fee Team website">
+                        <img class="header-logo" alt="" title="" src="/wp-content/themes/twentynineteen/landing-page-folder/img/logo-min.png">
+                        <!-- <img class="header-logo-2" alt="" title="" src="/wp-content/themes/twentynineteen/landing-page-folder/img/MHG-LOGO-no-effects.png"> -->
+                    </a>
+                </div>
+                <div class="col-12 col-sm-6 header-phone-number"><a href="tel:6024562906" id="header-phone-number-text">Questions? Call Now <span>(602) 456-2906</span></a></div>
+            </div>
+    </header>
+
+	<div id="content" class="site-content">
