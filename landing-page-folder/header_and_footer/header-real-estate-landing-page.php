20a6147810ddf3dce34b7380f3c72c8aa554950f
DonVentura <don@signamarketing.com>
2020-12-09 11:50:06 -0700

    Update: adding analytics

--- landing-page-folder/header_and_footer/header-real-estate-landing-page.php
+++ landing-page-folder/header_and_footer/header-real-estate-landing-page.php
@@ -18,9 +18,13 @@
 
 <head>
 
-  <!-- Google Tag Manager (noscript) -->
-  <!-- ANALYTICS GOES HERE -->
-  <!-- End Google Tag Manager (noscript) -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-52QZPFF');</script>
+<!-- End Google Tag Manager -->
 
   <title>Sell Your Home Now with Flat Fee Team AZ</title>
 
@@ -44,10 +48,10 @@
 
 <body <?php body_class(); ?>>
 
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52QZPFF"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52QZPFF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div id="page" class="site">
 
---
+++ landing-page-folder/real-estate-landing-page-2.php
@@ -0,0 +1,270 @@
+<!--
+Template Name: Real Estate Landing Page 2
+-->
+
+
+
+
+<?php require __DIR__ . "/header_and_footer/header-real-estate-landing-page.php"; ?>
+
+<!-- START OF CONTENT -->
+
+<div class="full-width-banner-container"
+     style="background: url(/wp-content/themes/twentytwenty/landing-page-folder/img/header-bg-img.jpg) no-repeat top center / cover;">
+
+     <div class="full-width-banner-row">
+
+          <div class="full-width-banner-box">
+
+               <h1 class="full-width-banner-heading" id="scroll-to-form">READY TO SELL YOUR PHOENIX HOME?</h1>
+
+               <div class="full-width-banner-subheading-box">
+
+                    <!-- mainly for positioning & constricting width of text -->
+
+                    <p class="full-width-banner-subheading-text">Find out how much your home is worth with a Free Home Price Valuation and save thousands</p>
+
+                    <!-- <p class="full-width-banner-subheading-text-3" style="font-size:27px;">Home of Our Flat $3950* Special</p> -->
+
+                    <div class="col-12"><?php echo do_shortcode('[8b_home_value]'); ?></div>
+
+               </div>
+
+          </div>
+
+     </div>
+
+</div>
+
+<!-- Column with icon above -->
+
+<div class="container-fluid">
+
+     <div class="container">
+
+          <div class="row testimonials ">
+
+               <div class="col-12 full-width-banner-subheading-text-2">
+
+                    <p class="" style="margin-top: 40px; text-align: center;">Sell Your Home For $3950*</p>
+                    <p style="text-align: center; margin-top: 10px; margin-bottom: 10px;">Meet The Flat Fee Team</p>
+
+               </div>
+
+               <div class="col-md-8 col-md-offset-3" style="margin: auto;">
+
+               <iframe id="vimeo-player-1" src="https://www.youtube.com/embed/4ywYH2-3YaI" width="640" height="360" frameborder="0" allowfullscreen="allowfullscreen" data-progress="”true”" data-seek="”true”" data-bounce="”false”"></iframe>
+
+               </div>
+
+               <div class="col-12 col-sm-12">
+
+                    <h2>Testimonials</h2>
+
+               </div>
+
+               <div class="col-md-6 testimonial-column">
+
+                    <p class="testimonial-column-text">“I was extremely fortunate to have met Cindy and Michael and had their expertise as my realtors when I was looking for a condo in Phoenix. Cindy and Michael are nothing short of amazing. They kept me constantly updated and I never had to run after them as I had with previous realtors!”</p>
+
+                    <div class="testimonial-person">
+
+                         <img src="/wp-content/themes/twentytwenty/landing-page-folder/img/testimonial-1.png" />
+                         <p>- Ellie Harris</p>
+
+                    </div>
+
+               </div>
+
+               <div class="col-md-6 testimonial-column">
+
+                    <p class="testimonial-column-text">“Cindy Valadez was a perfect match for us! She is extremely well-versed and connected in the industry. There was never a fear of the unknown with Cindy as our agent nor the process. As first time home buyers, she made the process easy and fun for us. We thank you again, Cindy!!”</p>
+
+                    <div class="testimonial-person">
+
+                         <img
+                              src="/wp-content/themes/twentytwenty/landing-page-folder/img/testimonial-placeholder.jpg" />
+                         <p>- Lisa Charisse</p>
+
+                    </div>
+
+               </div>
+
+          </div>
+
+     </div>
+
+</div>
+
+<div class="container-fluid">
+
+     <div class="row content-area-1">
+
+          <div class="col-md-6 ca1-column ca1-bg"
+               style="background-image: url(/wp-content/themes/twentytwenty/landing-page-folder/img/selling-your-house-min.jpg);">
+          </div>
+
+          <div class="col-md-6 ca1-column ca1-bg-2">
+
+               <div class="ca1-text">
+
+                    <h2>Selling your home can be easy.</h2>
+
+                    <p
+                         style="font-style: italic; font-weight: bold; margin-bottom: 15px; margin-top: 0px; line-height: 1.3;">
+                         Let our experienced realtors help you make it happen!</p>
+
+                         Sell Your Home For $3950* – At the Flat Fee Team, it’s your home, your equity, you keep more.
+
+
+
+               </div>
+
+               <div class="ca1-text">
+
+                    <h2 style="margin-top: 0px;">Benefits</h2>
+
+                    <div class="check-list">
+
+                         <ul>
+                              <li>All the normal services of a traditional agent
+(excellent representation, professional photos, yard signs, lock boxes, scheduling showings, marketing and advertising.)</li>
+                              <li>Your home is listed on the MLS, which syndicates your home onto thousands of websites including Zillow, Trulia and Realtor.com.</li>
+                              <li>And of course all of this for the flat rate of $3950*!</li>
+                         </ul>
+
+                    </div>
+
+               </div>
+
+               <div class="tx-center top-space-margin-1" style="clear: both; margin-bottom: 40px;">
+                    <a class="button" href="#scroll-to-form" style="text-transform: none;">Request Free Consultation</a>
+               </div>
+
+          </div><!-- end of column -->
+     
+
+     </div><!-- end of row -->
+
+</div><!-- end of container -->
+
+<div class="container-fluid">
+
+     <div class="container">
+
+          <div class="row content-area-2">
+
+               <div class="col-12">
+                    
+                    <h2>With the Flat Fee Team, you get more than just an Agent.</h2>
+
+                    <p>Our team leaders, Michael and Cindy, stay with you, helping you understand inspections, contracts, negotiating offers and closing. You don’t get handed over to a hourly “transaction manager” with our team.</p>
+
+                    <p>A traditional listing agent might charge you a 3% commission for all those services. On a $500,000 home sale, that adds up to $15,000! We offer the same service for only $3,950*!
+(*1% listing fee to apply of homes listed over $500k)</p>
+
+               </div>
+
+          </div>
+
+     </div>
+
+</div>
+
+<div class="container-fluid">
+
+     <div class="container">
+
+          <div class="row content-area-3">
+
+               <div class="col-sm-6 col-md-6 col-lg-4 ca3-col">
+
+                    <div class="panel-with-overlay"
+                         style="background: url('/wp-content/themes/twentytwenty/landing-page-folder/img/sold-house-1-min.jpg'); background-size: cover;">
+
+                         <div class="panel-overlay">
+                              <p class="panel-overlay-text">SOLD</p>
+                         </div>
+
+                    </div>
+
+               </div>
+
+               <div class="col-sm-6 col-md-6 col-lg-4 ca3-col">
+
+                    <div class="panel-with-overlay"
+                         style="background: url('/wp-content/themes/twentytwenty/landing-page-folder/img/sold-house-5-min.jpg'); background-size: cover;">
+
+                         <div class="panel-overlay">
+                              <p class="panel-overlay-text">SOLD</p>
+                         </div>
+
+                    </div>
+
+               </div>
+
+               <div class="col-sm-6 col-md-6 col-lg-4 ca3-col">
+
+                    <div class="panel-with-overlay"
+                         style="background: url('/wp-content/themes/twentytwenty/landing-page-folder/img/sold-house-6-min.jpg'); background-size: cover;">
+
+                         <div class="panel-overlay">
+                              <p class="panel-overlay-text">SOLD</p>
+                         </div>
+
+                    </div>
+
+               </div>
+
+          </div>
+
+<div class="col-12 content-area-2" style="
+
+            padding-bottom: 0;
+
+        ">
+
+        <h2>Meet The Team</h2>   
+
+           <img src="/wp-content/themes/twentytwenty/landing-page-folder/img/flat-fee-team-az-team-photo.jpg" scale="0" style="
+
+           width: 331px;
+           margin:auto;
+           max-width:100%;
+
+           -webkit-box-shadow: 5px 5px 6px 0px rgba(0,0,0,0.4);
+
+           -moz-box-shadow: 5px 5px 6px 0px rgba(0,0,0,0.4);
+
+           box-shadow: 5px 5px 6px 0px rgba(0,0,0,0.4);
+
+        ">
+
+        </div>
+
+     </div>
+
+</div>
+
+
+
+<div id="cta-row-1" class="container-fluid" style="padding-top: 48px;">
+
+     <div class="container">
+
+          <div class="row">
+
+               <div class="col-12"><a class="cta-btn-1" href="tel:6024562906">Ready to sell your home? <span
+                              style="font-weight: 800;">Call Now </span><b>(480) 200-2914</b></a></div>
+
+          </div>
+
+     </div>
+
+</div>
+
+<?php require __DIR__ . "/header_and_footer/footer-real-estate-landing-page.php"; ?>
+
+
+
+<!-- END OF CONTENT -->
